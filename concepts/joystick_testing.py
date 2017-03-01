5a2fec9b1e8516eef4622630782dff5d5e0827bc
Martin <martinr4@wit.edu>
2017-02-28 21:47:16 -0500

    add get_joystick_data function
    see docstring for more detail

--- concepts/joystick_testing.py
+++ concepts/joystick_testing.py
@@ -1,10 +1,10 @@
 import pygame
-from time import sleep
 
 AXIS_DECIMAL_PLACES = 3
 
 
 def setup_joysticks():
+    """returns list of Joystick objects, one for each joystick plugged in"""
     pygame.joystick.init()
     j = [pygame.joystick.Joystick(_) for _ in range(pygame.joystick.get_count())]
     for _ in j:
@@ -12,15 +12,27 @@ def setup_joysticks():
     return j
 
 
+def get_joystick_data(j):
+    """returns dictionary of all axis hat and button states of Joystick j"""
+    d = {}
+    for _ in range(j.get_numaxes()):
+        d['axis_{}'.format(_)] = j.get_axis(_)
+    for _ in range(j.get_numhats()):
+        d['hat_{}'.format(_)] = j.get_hat(_)
+    for _ in range(j.get_numbuttons()):
+        d['button_{}'.format(_)] = j.get_button(_)
+    return d
+
+
 def main():
     pygame.init()
+    clock = pygame.time.Clock()
     j = setup_joysticks()[0]    # assuming a joystick is plugged in
     print(j.get_id())
     while j:
         pygame.event.pump()
-        a = [str(round(j.get_axis(_), AXIS_DECIMAL_PLACES)) for _ in range(j.get_numaxes())]
-        print('\t\t'.join(a))   # print raw axis values in a rather disgusting manner
-        sleep(0.05)
+        print(get_joystick_data(j))
+        clock.tick(200)
 
 
 if __name__ == '__main__':
