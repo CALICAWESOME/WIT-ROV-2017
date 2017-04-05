b9e0fee4f3ce7698d0e01824a22a956dc2fda422
Martin <martinr4@wit.edu>
2017-04-04 23:10:08 -0400

    put everything in a class
    fuck global variables

--- surface_station/surface_run.py
+++ surface_station/surface_run.py
@@ -2,53 +2,57 @@ import pygame
 from flask import Flask, render_template
 surf = Flask(__name__)
 
-#TODO: make this into a class, global vars == bad
-AXIS_NUMDIGITS = 5
-CLOCK = None
-JOYSTICKS = []
 
+class SurfaceStation:
 
-def setup_joysticks():
-    """returns list of Joystick objects, one for each joystick plugged in"""
-    pygame.joystick.init()
-    j = [pygame.joystick.Joystick(_) for _ in range(pygame.joystick.get_count())]
-    for _ in j:
-        _.init()
-    return j
+    _axis_numdigits = 5
+    clock = None
+    joysticks = []
 
+    def __init__(self):
+        pygame.init()
+        self.clock = pygame.time.Clock()
+        self.joysticks = self._setup_joysticks()
 
-def get_joystick_data(j):
-    """returns dictionary of all axis hat and button states of Joystick j"""
-    d = {}
-    for _ in range(j.get_numaxes()):
-        d['axis_{}'.format(_)] = round(j.get_axis(_), 5)
-    for _ in range(j.get_numhats()):
-        d['hat_{}'.format(_)] = j.get_hat(_)
-    for _ in range(j.get_numbuttons()):
-        d['button_{}'.format(_)] = j.get_button(_)
-    return d
+    @property
+    def axis_numdigits(self):
+        return self._axis_numdigits
 
+    @staticmethod
+    def _setup_joysticks():
+        """returns list of Joystick objects, one for each joystick plugged in"""
+        pygame.joystick.init()
+        j = [pygame.joystick.Joystick(_) for _ in range(pygame.joystick.get_count())]
+        for _ in j:
+            _.init()
+        return j
 
-def setup():
-    global CLOCK
-    global JOYSTICKS
-    pygame.init()
-    CLOCK = pygame.time.Clock()
-    JOYSTICKS = setup_joysticks()
+    def get_joystick_data(self):
+        """returns dictionary of all axis hat and button states of Joystick j"""
+        _ = []
+        for j in self.joysticks:
+            d = {}
+            for a in range(j.get_numaxes()):
+                d['axis_{}'.format(a)] = round(j.get_axis(a), self._axis_numdigits)
+            for h in range(j.get_numhats()):
+                d['hat_{}'.format(h)] = j.get_hat(h)
+            for b in range(j.get_numbuttons()):
+                d['button_{}'.format(b)] = j.get_button(b)
+            _.append(d)
+        return _
 
-
-def loop():
-    pygame.event.pump()
-    print(get_joystick_data(JOYSTICKS[0]))
-    CLOCK.tick(200)
+    def tick(self):
+        pygame.event.pump()
+        print(self.get_joystick_data())
+        self.clock.tick(200)
 
 
 @surf.route('/')
 def dataview():
-    loop()
-    return render_template('index.html', data=get_joystick_data(JOYSTICKS[0]))
+    s = SurfaceStation()
+    s.tick()
+    return render_template('index.html', data=s.get_joystick_data()[0])
 
 
 if __name__ == '__main__':
-    setup()
     surf.run(port=8000, debug=True)
